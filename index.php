a5081b58cf772984a8897a798bc93f6cec5a5aa9
pwa02747-create <pwa02747@gmail.com>
2025-10-30 18:16:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+require_once 'conexion.php';
+
+$db = new Database();
+
+$sql = "INSERT INTO vehiculos (placa, marca, modelo) VALUES (:placa, :marca, :modelo)";
+$params = [
+  ':placa' => 'XYZ-999',
+  ':marca' => 'Nissan',
+  ':modelo' => 'Versa'
+];
+$id = $db->execute($sql, $params, true);
+
+echo json_encode(["success" => true, "vehiculo_id" => $id]);
