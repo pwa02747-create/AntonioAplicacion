92d04ddb2546b76ff5521784013d294829272e34
pwa02747-create <pwa02747@gmail.com>
2025-10-30 19:29:04 -0500

    Add files via upload

---
+++ JWT/config/index.php
@@ -0,0 +1,45 @@
+<?php
+
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Allow: GET, POST, OPTIONS");
+
+// Manejar preflight (opcional, pero recomendado)
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    http_response_code(200);
+    exit();
+}
+
+date_default_timezone_set("America/Matamoros");
+
+require "conexion.php";
+require "enviarCorreo.php";
+# mkdir firebase-php-jwt
+# cd firebase-php-jwt
+# composer require firebase/php-jwt
+require "firebase-php-jwt/vendor/autoload.php";
+# mkdir kreait-firebase-php
+# cd kreait-firebase-php
+# composer require kreait/firebase-php
+# extensiones php -----> curl, json, openssl, mbstring o gmp
+require "kreait-firebase-php/vendor/autoload.php";
+
+$jwtKey = "Test12345";
+
+$serviceAccountJson = json_decode('', true);
+$firebase = (new Kreait\Firebase\Factory)->withServiceAccount($serviceAccountJson)->createMessaging();
+
+$con = new Conexion(array(
+    "tipo"       => "mysql",
+    "servidor"   => "localhost",
+    "bd"         => "practicas",
+    "usuario"    => "root",
+    "contrasena" => "Test12345"
+));
+
+?>
